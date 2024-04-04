8238d3a10cd9b2119292718e5537daa6359f3be5
Usman Khan <usman918khan@gmail.com>
2024-04-04 21:05:06 +0500

    (firstJsProgram)

---
+++ Basic 14 Js Programs/script.js
@@ -0,0 +1,14 @@
+//1. A js program to find the maximum between two numbers.
+
+/*
+let number1 = prompt("enter number1");
+let number2 = prompt("enter number2");
+if (number1 > number2) {
+    alert(`${number1} is max`)
+    
+} else if (number1 == number2) {
+    alert(`both are equal`)
+} else {
+    alert(`${number2} is max`)
+}  
+*/ 
