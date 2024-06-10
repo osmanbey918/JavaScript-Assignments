0dae87079c35c9de51941f36e941703655688658
Usman Khan <usman918khan@gmail.com>
2024-06-10 23:23:18 +0500

    calculatorUpdating

--- Calculator/script.js
+++ Calculator/script.js
@@ -3,27 +3,30 @@ function getNvalue(button) {
     let value = parseInt(button.value);
     display += value
     console.log(display)
-   document.getElementById('display').innerHTML = display;
+    document.getElementById('display').innerHTML = display;
 }
 function op(op) {
-    
-    display += op.value
-console.log(display)
-document.getElementById('display').innerHTML = display
+
+    if(display[display.length - 1] !== '+'  && display[display.length - 1] !== '-'  && display[display.length - 1] !== '/'  && display[display.length - 1] !== '*' ){
+
+        display += op.value
+        console.log(display)
+        document.getElementById('display').innerHTML = display
+    }
 }
 
 function clr(clr) {
     display = ''
-document.getElementById('display').innerHTML = '0'
+    document.getElementById('display').innerHTML = '0'
 
 }
 
 
 function getans() {
-        eval(display);
-       let dis = eval(display)
-     console.log(dis)
-document.getElementById('display').innerHTML = dis
-     
+    eval(display);
+    let dis = eval(display)
+    console.log(dis)
+    document.getElementById('display').innerHTML = dis
+
 
 }
