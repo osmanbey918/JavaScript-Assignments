803f2fa528ae635d65ecbf48379ec0d7e14ce749
Usman Khan <usman918khan@gmail.com>
2024-06-10 15:35:06 +0500

    projectCalculator

---
+++ Calculator/script.js
@@ -0,0 +1,10 @@
+var display = ''
+function getNvalue(button) {
+    let value = parseInt(button.value);
+    display += value
+    console.log(display)
+}
+function getans() {
+    let res = display
+    console.log(res)
+}
