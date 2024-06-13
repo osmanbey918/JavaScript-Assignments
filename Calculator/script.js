96f4ff6706dfad34e8dbe962c5f871355c2f7e31
Usman Khan <usman918khan@gmail.com>
2024-06-13 18:13:51 +0500

    updating

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,4 +1,4 @@
-var display = ''
+/*var display = ''
 function getNvalue(button) {
     let value = parseInt(button.value);
     display += value
@@ -27,4 +27,59 @@ function getans() {
     let dis = eval(display)
     console.log(dis)
     document.getElementById('display').innerHTML = dis
+}
+
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const button1 = document.getElementById('button1');
+    const button2 = document.getElementById('button2');
+
+    button1.addEventListener('click', function() {
+        button1.classList.add('hidden');
+        button2.classList.remove('hidden');
+    });
+
+    button2.addEventListener('click', function() {
+        button2.classList.add('hidden');
+        button1.classList.remove('hidden');
+    });
+});
+*/
+
+
+
+
+
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function backspace() {
+    let display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    let display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
+
+
+function toggleScientific() {
+    const scientificButtons = document.querySelector('.scientific-buttons');
+    scientificButtons.classList.toggle('hidden');
 }
